90dc9402a2a693c677070df8dad996fae3f0e098
alexquar <alexanderquarrie@gmail.com>
2025-07-24 00:26:38 -0400

    binary operators setup, allocator arena mem

---
+++ src/arena.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+class ArenaAllocator {
+public:
+    inline ArenaAllocator(size_t size)
+        : m_size(size)
+    {
+        m_buffer = new(std::byte[size]);
+        m_offset = m_buffer;
+    }
+    template<typename T>
+    inline T* alloc()
+    {
+        std::byte* offset = m_offset;
+        m_offset += sizeof(T);
+        return offset;
+    }
+    inline ArenaAllocator(const ArenaAllocator& other) = delete;
+    inline ArenaAllocator& operator=(const ArenaAllocator& other) = delete;
+    inline ~ArenaAllocator()
+    {
+        ::operator delete(m_buffer);
+    }
+
+private:
+    size_t m_size;
+    std::byte* m_buffer;
+    std::byte* m_offset;
+};
--- src/parser.hpp
+++ src/parser.hpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <optional>
 #include <variant>
-
+#include "arena.hpp"
 
 struct NodeExprIdent{
     Token ident;
@@ -14,21 +14,41 @@ struct NodeExprIntLit {
     Token int_lit;
 };
 
-struct NodeExpr {
-    std::variant<NodeExprIntLit, NodeExprIdent> var;
-};
+struct NodeExpr;
 struct NodeStmtExit {
-    NodeExpr expr;
+    NodeExpr* expr;
 };
 
+struct BinExprAdd {
+    NodeExpr left;
+    NodeExpr right;
+};
+
+struct BinExprMultiply {
+    NodeExpr* left;
+    NodeExpr* right;
+};
+
+struct BinExprMultiply {
+    NodeExpr* left;
+    NodeExpr* right;
+};
+
+struct BinExpr {
+    std::variant<BinExprAdd, BinExprMultiply> var;
+};
+
+struct NodeExpr {
+    std::variant<NodeExprIntLit*, NodeExprIdent*, BinExpr*> var;
+};
 
 struct NodeStmtMake {
     Token ident;
-    NodeExpr expr;
+    NodeExpr* expr;
 };
 
 struct NodeStmt{
-    std::variant<NodeStmtExit, NodeStmtMake> expr;
+    std::variant<NodeStmtExit*, NodeStmtMake*> expr;
 };
 struct NodeProg{
     std::vector<NodeStmt> stmts;
@@ -36,16 +56,20 @@ struct NodeProg{
 
 
 
+
+
 class Parser {
 public:
     inline explicit Parser(std::vector<Token> tokens)
-        : m_tokens(std::move(tokens))
+        : m_tokens(std::move(tokens)),
+            m_allocator(1024 * 1024 * 4)
     {
     }
 
     std::optional<NodeExpr> parse_expr()
     {
         if (peek().has_value() && peek().value().type == TokenType::int_lit) {
+            //next code goes here
             return NodeExpr{ NodeExprIntLit{ consume() } };
         }
         else if(peek().has_value() && peek().value().type == TokenType::ident) {
@@ -145,4 +169,5 @@ private:
 
     const std::vector<Token> m_tokens;
     size_t m_index = 0;
+    ArenaAllocator m_allocator;
 };
